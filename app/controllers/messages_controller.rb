b4706e632c78fd35c5fac02d1d1be4e1218c62a8
jimbitol <cabreram.815@gmail.com>
2017-01-16 03:32:41 +1100

    form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,19 @@
 class MessagesController < ApplicationController
 
 	def index
+		@message = Message.new
 		@messages = Message.find_by_private(false)
 	end
 
 	def create
+		@message = Message.create(message_params)
+
+	    respond_to do |format|
+	      format.js
+	    end
+	end
+
+	def show
 
 	end
 end
